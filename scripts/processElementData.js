0af3a0c5a5a701d8a215438dd8b37d0a5cfa7599
IvaN DiniZ <ivandiniz@gmail.com>
2016-06-09 10:51:53 -0300

    publicação do processo

---
+++ scripts/processElementData.js
@@ -0,0 +1,70 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_7NIDIBOVEeaMab92HdiewQ,_YjjuNRkTEeaI7-nxU7ipwQActivity"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Distribuir Chamado_D3AF4533_f88a835f_Activity.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_7NIDIBOVEeaMab92HdiewQ,_YjjuNRkTEeaI7-nxU7ipwQActivityDetail"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Distribuir Chamado_D3AF4533_f88a835f_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_6TLY0ROUEeaMab92HdiewQ,_BUJcAROnEeaoF8c2YfdgvQActivity"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Distribuir Chamado_A8CCD515_1337ef62_Activity.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_6TLY0ROUEeaMab92HdiewQ,_BUJcAROnEeaoF8c2YfdgvQActivityDetail"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Distribuir Chamado_A8CCD515_1337ef62_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQActivity"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/processo_atendimento_chamados_ifrn_D2D22B9B_d2d22b9b_Activity.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_Nb9yIBOWEeaMab92HdiewQActivity"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Homologacao_1581928C_88c5eb7b_Activity.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_Nb9yIBOWEeaMab92HdiewQActivityDetail"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Homologacao_1581928C_88c5eb7b_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_7NIDIBOVEeaMab92HdiewQActivity"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Atendimento_8F865EE9_2cab7d8_Activity.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_7NIDIBOVEeaMab92HdiewQActivityDetail"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Atendimento_8F865EE9_2cab7d8_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_7NIDIBOVEeaMab92HdiewQ,_t-qsshO0EeaoF8c2YfdgvQActivity"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Atender Chamado_AB4632EA_d0217116_Activity.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_7NIDIBOVEeaMab92HdiewQ,_t-qsshO0EeaoF8c2YfdgvQActivityDetail"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Atender Chamado_AB4632EA_d0217116_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_6TLY0ROUEeaMab92HdiewQActivity"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Distribuicao_68F94E0A_dc3da6f9_Activity.jpeg"
+contentPage.processPage.imageFiles["_g0ubARB_EeaMab92HdiewQ,_6TLY0ROUEeaMab92HdiewQActivityDetail"]="./../../processo_atendimento_chamados_ifrn/deliveryprocesses/resources/Distribuicao_68F94E0A_dc3da6f9_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_dTsb0BPCEeaoF8c2YfdgvQ"] = ["Alterar Chamado para Resolvido","processo_atendimento_chamados_ifrn/tasks/status_resolvido_BC92667B.html"];
+contentPage.processPage.elementUrls["_o36pwRO-EeaoF8c2YfdgvQ"] = ["Cadastrar Comentário","processo_atendimento_chamados_ifrn/tasks/cadastrar_comentario_9A736B65.html"];
+contentPage.processPage.elementUrls["_PcVuMRkXEeaI7-nxU7ipwQ"] = ["Analisar Problema Informado","processo_atendimento_chamados_ifrn/tasks/analisar_problema_informado_A384C1A2.html"];
+contentPage.processPage.elementUrls["_Di_nQROpEeaoF8c2YfdgvQ"] = ["Identificar Serviço Solicitado","processo_atendimento_chamados_ifrn/tasks/identificar_servico_solicitado_33F7AA18.html"];
+contentPage.processPage.elementUrls["_fW0QgBPKEeaoF8c2YfdgvQ"] = ["Fechar Chamado","processo_atendimento_chamados_ifrn/tasks/fechar_chamado_4A8B46E5.html"];
+contentPage.processPage.elementUrls["_YjjuNRkTEeaI7-nxU7ipwQ"] = ["Distribuir Chamado","processo_atendimento_chamados_ifrn/deliveryprocesses/Distribuir Chamado_D3AF4533.html"];
+contentPage.processPage.elementUrls["_Nb9yIBOWEeaMab92HdiewQ"] = ["Homologação","processo_atendimento_chamados_ifrn/deliveryprocesses/Homologacao_1581928C.html"];
+contentPage.processPage.elementUrls["_pty74R4REeaG6rLFhql7rA"] = ["Atendente","processo_atendimento_chamados_ifrn/roles/atendente_D2297EF9.html"];
+contentPage.processPage.elementUrls["_BI7PMBPCEeaoF8c2YfdgvQ"] = ["Cadastrar Solução na Base de Conhecimentos","processo_atendimento_chamados_ifrn/tasks/cadastrar_base_de_conhecimentos_F8268F02.html"];
+contentPage.processPage.elementUrls["_ki2aoR4JEeaG6rLFhql7rA"] = ["SUAP","processo_atendimento_chamados_ifrn/roles/suap_3E0EBAB5.html"];
+contentPage.processPage.elementUrls["_a3nEoBOmEeaoF8c2YfdgvQ"] = ["Abrir Chamado","processo_atendimento_chamados_ifrn/tasks/abrir_chamado_E63B3CB0.html"];
+contentPage.processPage.elementUrls["_7NIDIBOVEeaMab92HdiewQ"] = ["Atendimento","processo_atendimento_chamados_ifrn/deliveryprocesses/Atendimento_8F865EE9.html"];
+contentPage.processPage.elementUrls["_9bsGQBOyEeaoF8c2YfdgvQ"] = ["Atribuir Chamado","processo_atendimento_chamados_ifrn/tasks/atribuir_chamado_4910047F.html"];
+contentPage.processPage.elementUrls["_t1RHkR4REeaG6rLFhql7rA"] = ["Interessado","processo_atendimento_chamados_ifrn/roles/interessado_FAB07290.html"];
+contentPage.processPage.elementUrls["_1l8jtB4TEeaG6rLFhql7rA"] = ["Interessado","processo_atendimento_chamados_ifrn/roles/interessado_FAB07290.html"];
+contentPage.processPage.elementUrls["_YtYvEBbjEea9TrfubCHhXg"] = ["Reclassificar Chamado","processo_atendimento_chamados_ifrn/tasks/reclassificar_chamado_87FEF661.html"];
+contentPage.processPage.elementUrls["_t-qsshO0EeaoF8c2YfdgvQ"] = ["Atender Chamado","processo_atendimento_chamados_ifrn/deliveryprocesses/Atender Chamado_AB4632EA.html"];
+contentPage.processPage.elementUrls["_YjjuMRkTEeaI7-nxU7ipwQ"] = ["Distribuir Chamado para Central de Atendimento Responsável","processo_atendimento_chamados_ifrn/tasks/distribuir_chamado_para_central_de_atendimento_responsavel_8AD3FF5F.html"];
+contentPage.processPage.elementUrls["_rvx_gRxVEeaG6rLFhql7rA"] = ["Atendente","processo_atendimento_chamados_ifrn/roles/atendente_D2297EF9.html"];
+contentPage.processPage.elementUrls["_jitsMROpEeaoF8c2YfdgvQ"] = ["Distribuir Chamado para Central de Atendimento Responsável","processo_atendimento_chamados_ifrn/tasks/distribuir_chamado_para_central_de_atendimento_responsavel_8AD3FF5F.html"];
+contentPage.processPage.elementUrls["_K2gLgBkXEeaI7-nxU7ipwQ"] = ["Analisar a Solicitação do Interessado","processo_atendimento_chamados_ifrn/tasks/analisar_a_solicitacao_do_interessado_8C217585.html"];
+contentPage.processPage.elementUrls["_rvx_hBxVEeaG6rLFhql7rA"] = ["Interessado","processo_atendimento_chamados_ifrn/roles/interessado_FAB07290.html"];
+contentPage.processPage.elementUrls["_rvx_hxxVEeaG6rLFhql7rA"] = ["Responsável por Grupo de Atendimento","processo_atendimento_chamados_ifrn/roles/responsavel_grupo_95E5AED4.html"];
+contentPage.processPage.elementUrls["_xsBE8BblEea9TrfubCHhXg"] = ["Retornar Chamado","processo_atendimento_chamados_ifrn/tasks/retornar_chamado_D3230705.html"];
+contentPage.processPage.elementUrls["_9UqucR4JEeaG6rLFhql7rA"] = ["Atendente","processo_atendimento_chamados_ifrn/roles/atendente_D2297EF9.html"];
+contentPage.processPage.elementUrls["_Z3XnQBO0EeaoF8c2YfdgvQ"] = ["Escalar Chamado","processo_atendimento_chamados_ifrn/tasks/escalar_chamado_E4C3BF89.html"];
+contentPage.processPage.elementUrls["_MitwwBOoEeaoF8c2YfdgvQ"] = ["Identificar Interessado","processo_atendimento_chamados_ifrn/tasks/identificar_interessado_61465DCA.html"];
+contentPage.processPage.elementUrls["_9lm2QRO9EeaoF8c2YfdgvQ"] = ["Realizar Atendimento","processo_atendimento_chamados_ifrn/tasks/realizar_atendimento_88747EC5.html"];
+contentPage.processPage.elementUrls["__C_bQROyEeaoF8c2YfdgvQ"] = ["Assumir Chamado","processo_atendimento_chamados_ifrn/tasks/assumir_chamado_15A9F18B.html"];
+contentPage.processPage.elementUrls["_4Jo0AR4JEeaG6rLFhql7rA"] = ["Responsável por Grupo de Atendimento","processo_atendimento_chamados_ifrn/roles/responsavel_grupo_95E5AED4.html"];
+contentPage.processPage.elementUrls["_dMFd4BkmEeaTKPMXZkcAAg"] = ["Consultar Base de Conhecimento","processo_atendimento_chamados_ifrn/tasks/consultar_base_de_conhecimento_9106506E.html"];
+contentPage.processPage.elementUrls["_QPu2QRPCEeaoF8c2YfdgvQ"] = ["Escolher Solução na Base de Conhecimentos","processo_atendimento_chamados_ifrn/tasks/escolher_base_de_conhecimentos_130ED4C7.html"];
+contentPage.processPage.elementUrls["_X8x1gR4KEeaG6rLFhql7rA"] = ["SUAP","processo_atendimento_chamados_ifrn/roles/suap_3E0EBAB5.html"];
+contentPage.processPage.elementUrls["_1l8jsR4TEeaG6rLFhql7rA"] = ["Responsável por Grupo de Atendimento","processo_atendimento_chamados_ifrn/roles/responsavel_grupo_95E5AED4.html"];
+contentPage.processPage.elementUrls["_g0ubARB_EeaMab92HdiewQ"] = ["Atendimento Central de Serviços SUAP","processo_atendimento_chamados_ifrn/deliveryprocesses/processo_atendimento_chamados_ifrn_D2D22B9B.html"];
+contentPage.processPage.elementUrls["_K8NPcBbpEea9TrfubCHhXg"] = ["Atualizar Status para Suspenso","processo_atendimento_chamados_ifrn/tasks/status_suspenso_C2F7C9F7.html"];
+contentPage.processPage.elementUrls["_6TLY0ROUEeaMab92HdiewQ"] = ["Distribuição","processo_atendimento_chamados_ifrn/deliveryprocesses/Distribuicao_68F94E0A.html"];
+contentPage.processPage.elementUrls["_YgasUBO9EeaoF8c2YfdgvQ"] = ["Atualizar Status para Em Atendimento","processo_atendimento_chamados_ifrn/tasks/status_em_atendimento_418EBD9.html"];
+contentPage.processPage.elementUrls["_Z1f9ABPKEeaoF8c2YfdgvQ"] = ["Reabrir Chamado","processo_atendimento_chamados_ifrn/tasks/reabrir_chamado_CD0E96CB.html"];
+contentPage.processPage.elementUrls["_xOwy8R4REeaG6rLFhql7rA"] = ["Responsável por Grupo de Atendimento","processo_atendimento_chamados_ifrn/roles/responsavel_grupo_95E5AED4.html"];
+contentPage.processPage.elementUrls["_BUJcAROnEeaoF8c2YfdgvQ"] = ["Distribuir Chamado","processo_atendimento_chamados_ifrn/deliveryprocesses/Distribuir Chamado_A8CCD515.html"];
+contentPage.processPage.elementUrls["_YjjuNhkTEeaI7-nxU7ipwQ"] = ["Identificar Interessado","processo_atendimento_chamados_ifrn/tasks/identificar_interessado_61465DCA.html"];
+contentPage.processPage.elementUrls["_YjjuMxkTEeaI7-nxU7ipwQ"] = ["Identificar Serviço Solicitado","processo_atendimento_chamados_ifrn/tasks/identificar_servico_solicitado_33F7AA18.html"];
+contentPage.processPage.elementUrls["_9MgTIBk2EeaUbOzn_w0V4A"] = ["Avaliar Atendimento","processo_atendimento_chamados_ifrn/tasks/avaliar_atendimento_A55107CE.html"];
